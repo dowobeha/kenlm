488851bec1bf6af35bfa7254fc5e310aef4b9847
Lane Schwartz <dowobeha@gmail.com>
2013-12-24 08:11:49 -0600

    Added running example to query_interp

--- lm/query_interp_main.cc
+++ lm/query_interp_main.cc
@@ -37,7 +37,14 @@ int main(int argc, char *argv[]) {
   
   // Iterate through all n-grams
   for (std::string line; std::getline(std::cin, line); ) {
-    
+
+	  
+	// For clarity, we provide a running example.
+	// Assume an example input line: 
+	//
+	// line = "a b c d e"
+	  
+	  
     // Parse line into a vector of words
     std::vector<std::string> words; {
       std::stringstream stream(line);
@@ -46,21 +53,48 @@ int main(int argc, char *argv[]) {
         words.push_back(word);
       }
     }
-    
+
+	  
+	// Continue the running example:
+	//
+	// words[0] = "a"
+	// words[1] = "b"
+	// words[2] = "c"
+	// words[3] = "d"
+	// words[4] = "e"	  
+	  
+	  
     // Collect word indices in a reverse-ordered array
     unsigned int num_words = words.size();
     WordIndex indices[num_words + 1];
     for (unsigned int i = 0; i < num_words; ++i) {
       indices[num_words - 1 - i] = vocab.Index(words[i]);
     }
+	  
+	// Continue the running example:
+	//
+	// num_words = 5
+	//
+	// indices[5] is not initialized
+	// indices[4] = a
+	// indices[3] = b
+	// indices[2] = c
+	// indices[1] = d
+	// indices[0] = e
+	
     
     // Output variable, which we will ignore
-    State nextState;
-    
+    State nextState;  
+	  
     // Query the LM
     FullScoreReturn ret = 
       model.FullScoreForgotState(&(indices[1]), &(indices[num_words]), indices[0], nextState);
     
+	// Continue the running example:
+	//
+	// The call to FullScoreForgotState obtains p( e | a b c d )
+	  
+	  
     // Report the score
     std::cout << ret.prob << std::endl;
     
